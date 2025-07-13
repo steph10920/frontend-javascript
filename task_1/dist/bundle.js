c032f41e2bf5802bad522ab9175ac2f12b06a194
Steph10920 <sashioyas@gmail.com>
2025-07-13 20:05:24 +0300

    added new changes

---
+++ task_1/dist/bundle.js
@@ -0,0 +1,33 @@
+/*
+ * ATTENTION: The "eval" devtool has been used (maybe by default in mode: "development").
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+/******/ (() => { // webpackBootstrap
+/******/ 	"use strict";
+/******/ 	var __webpack_modules__ = ({
+
+/***/ "./js/main.ts":
+/*!********************!*\
+  !*** ./js/main.ts ***!
+  \********************/
+/***/ (() => {
+
+eval("{\r\n// Example usage\r\nvar teacher3 = {\r\n    firstName: 'John',\r\n    fullTimeEmployee: false,\r\n    lastName: 'Doe',\r\n    location: 'London',\r\n    contract: false,\r\n};\r\nconsole.log(teacher3);\r\nvar director1 = {\r\n    firstName: 'John',\r\n    lastName: 'Doe',\r\n    location: 'London',\r\n    fullTimeEmployee: true,\r\n    numberOfReports: 17,\r\n};\r\nconsole.log(director1);\r\n\n\n//# sourceURL=webpack://task_0/./js/main.ts?\n}");
+
+/***/ })
+
+/******/ 	});
+/************************************************************************/
+/******/ 	
+/******/ 	// startup
+/******/ 	// Load entry module and return exports
+/******/ 	// This entry module can't be inlined because the eval devtool is used.
+/******/ 	var __webpack_exports__ = {};
+/******/ 	__webpack_modules__["./js/main.ts"]();
+/******/ 	
+/******/ })()
+;
--- task_1/js/main.ts
+++ task_1/js/main.ts
@@ -1,12 +1,19 @@
+// Define the Teacher interface
 interface Teacher {
   readonly firstName: string;
   readonly lastName: string;
   fullTimeEmployee: boolean;
   yearsOfExperience?: number;
   location: string;
-  [key: string]: any; // Allows additional properties like "contract"
+  [key: string]: any; // Allow extra properties
 }
 
+// Extend Teacher to create Directors interface
+interface Directors extends Teacher {
+  numberOfReports: number;
+}
+
+// Example usage
 const teacher3: Teacher = {
   firstName: 'John',
   fullTimeEmployee: false,
@@ -16,3 +23,13 @@ const teacher3: Teacher = {
 };
 
 console.log(teacher3);
+
+const director1: Directors = {
+  firstName: 'John',
+  lastName: 'Doe',
+  location: 'London',
+  fullTimeEmployee: true,
+  numberOfReports: 17,
+};
+
+console.log(director1);
